eb6a4c9409a6b2ccac4398dda9b842c91f121da0
Anastasiya <v.anastasiya26@mail.ru>
2020-02-27 23:14:22 +0700

    задание 16: redux, reducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import App from "./components/app/app.jsx";
 import questions from "./mocks/questions.js";
+import {reducer} from "./reducer.js";
 
-const errorsAmount = 3;
+const Settings = {
+  ERRORS_COUNT: 3
+};
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
-    <App
-      errorsAmount={errorsAmount}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsAmount={Settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.getElementById(`root`)
 );
